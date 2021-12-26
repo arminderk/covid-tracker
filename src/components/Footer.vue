ada10481714253a0f56f63b5dbbf549bb21f2a8a
Arminder Khinda <arminder.khinda@yahoo.com>
2021-12-26 14:27:54 -0800

    Covid Tracker app

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="text-center bg-blue-800 text-white p-5 mt-10">
+    <i class="fas fa-copyright"></i> By Arminder
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer'
+  }
+</script>
